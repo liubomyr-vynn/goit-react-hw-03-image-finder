fab0aec0898e99d1e5f74a418a329d58d2c532df
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-14 23:57:14 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
-  // state = {
-  //   currentPage: 1,
-  // };
-
-  // handleLoadMore = () => {
-  //   this.setState({ currentPage: this.state.currentPage + 1 });
-  //   console.log(this.state.currentPage);
-  // };
-
   render() {
     return (
       <div className="container">
-        <button onClick={this.props.onClick} className="Button" type="button">
+        <button onClick={this.props.onChange} className="Button" type="button">
           Load more
         </button>
       </div>

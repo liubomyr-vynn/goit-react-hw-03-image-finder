3562ae441ad24d0f2700712f31f02f906eab1526
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-14 23:56:23 +0300

    Update Gallery-api.js

--- src/Services/Gallery-api.js
+++ src/Services/Gallery-api.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '34845172-53e67b2f5b8cb8ccf3124ff1b';
 
-const fetchImages = (query, page = 1) => {
+const fetchImages = (query, page) => {
   return fetch(
     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12
 

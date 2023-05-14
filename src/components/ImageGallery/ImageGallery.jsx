b63b397dda354ecd2446c085efc47cf9f6891619
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-14 23:57:30 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,44 +2,73 @@ import React, { Component } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
 import imagesApi from '../../Services/Gallery-api';
+import { nanoid } from 'nanoid';
 
 class ImageGallery extends Component {
   state = {
     searchQuery: '',
     currentPage: 1,
+    images: [],
   };
 
-  componentDidUpdate(prevProps) {
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1,
+    }));
+  };
+
+  componentDidUpdate(prevProps, prevState) {
     const prevInput = prevProps.inputValue;
     const nextInput = this.props.inputValue;
+    const prevStatePage = prevState.currentPage;
+    const nextStatePage = this.state.currentPage;
 
     if (prevInput !== nextInput) {
+      this.setState({
+        searchQuery: '',
+        images: [],
+        currentPage: 1,
+      });
+
       imagesApi
-        .fetchImages(nextInput)
-        .then(searchQuery => this.setState({ searchQuery }))
+        .fetchImages(nextInput, 1)
+        .then(data => {
+          const newImages = data.hits;
+          this.setState({
+            searchQuery: data,
+            images: newImages,
+          });
+        })
+        .catch(error => console.log(error));
+    } else if (prevStatePage !== nextStatePage) {
+      imagesApi
+        .fetchImages(nextInput, nextStatePage)
+        .then(data => {
+          const newImages = data.hits;
+          this.setState(prevState => ({
+            searchQuery: data,
+            images: [...prevState.images, ...newImages],
+          }));
+        })
         .catch(error => console.log(error));
     }
   }
 
-  handleLoadMore = () => {
-    this.setState({ currentPage: this.state.currentPage + 1 });
-    console.log(this.state.currentPage);
-  };
-
   render() {
-    const { searchQuery } = this.state;
+    const { searchQuery, images } = this.state;
     return (
       <div>
         <ul className="ImageGallery">
           {searchQuery !== '' &&
-            searchQuery.hits.map(image => (
+            images.map(image => (
               <ImageGalleryItem
+                key={nanoid()}
                 largeImage={image.largeImageURL}
                 webformat={image.webformatURL}
               />
             ))}
         </ul>
-        {searchQuery !== '' && <Button onClick={this.handleLoadMore} />}
+        {searchQuery !== '' && <Button onChange={this.handleLoadMore} />}
       </div>
     );
   }
